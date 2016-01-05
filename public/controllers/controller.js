5b357e67620f6dbbdd38f619bc834b6dd91a28d1
Kelly Rhodes <krhodes1@fullsail.edu>
2016-01-05 13:53:04 -0500

    Simple MEAN stack with full CRUD

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,11 +3,47 @@ angular.module('myApp', [])
 	
 	console.log("Hello World from Controller 1!");	
 
-	$http.get('/songlist').success(function(response){
-		console.log("I got the data I requested");
-		$scope.songlist = response;
-	});
+	var refresh = function(){
+		$http.get('/songlist').success(function(response){
+			console.log("I got the data I requested");
+			$scope.songlist = response;
+			$scope.song = "";
+		});
+	};
 
-	
+	refresh();	
+
+	$scope.addSong = function(){
+		console.log($scope.song);
+		$http.post('/songlist', $scope.song).success(function(response){
+			console.log(response);
+			refresh();
+		});
+	};
+
+	$scope.removeSong = function(id){
+		console.log(id);
+		$http.delete('/songlist/' + id).success(function(response){
+			refresh();
+		});
+	};
+
+	$scope.editSong = function(id){
+		console.log(id);
+		$http.get('/songlist/' + id).success(function(response){
+			$scope.song = response;
+		});
+	};
+
+	$scope.updateSong = function(){
+		console.log($scope.song._id);
+		$http.put('/songlist/' + $scope.song._id, $scope.song).success(function(response){
+			refresh();
+		});
+	};
+
+	$scope.deselectSong = function(){
+		$scope.song = "";
+	}
 
 }]);	
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express        	= require('express');
 var path 			= require('path');
 var app            	= express();
 var mongojs			= require('mongojs');
-// var bodyParser     	= require('body-parser');
+var bodyParser     	= require('body-parser');
 // var methodOverride 	= require('method-override');
 
 
@@ -14,9 +14,9 @@ var db = mongojs('songlist', ['songlist']);
 // set our port
 var port = process.env.PORT || 7000; 
 
-// get all data/stuff of the body (POST) parameters
+// get all data of the body (POST) parameters
 // parse application/json
-// app.use(bodyParser.json()); 
+app.use(bodyParser.json()); 
 
 // parse application/vnd.api+json as json 
 // app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
@@ -30,7 +30,7 @@ var port = process.env.PORT || 7000;
 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public')); 
 
-app.get('*', function (req, res) {
+app.get('/songlist', function (req, res) {
     console.log("I received a GET request");
 
     db.songlist.find(function(err,docs){
@@ -38,10 +38,47 @@ app.get('*', function (req, res) {
     	res.json(docs);
     });
 
-
-
     //res.sendfile('public/index.html');
 })
+
+app.post('/songlist', function(req, res){
+	console.log(req.body);
+	db.songlist.insert(req.body, function(err,doc){
+		res.json(doc);
+	});
+});
+
+app.delete('/songlist/:id', function(req,res){
+	var id = req.params.id;
+	console.log(id);
+	db.songlist.remove({_id:mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);
+	})
+});
+
+app.get('/songlist/:id', function(req,res){
+	var id = req.params.id;
+	console.log(id);
+	db.songlist.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);
+	});
+});
+
+app.put('/songlist/:id', function(req,res){
+	var id = req.params.id;
+	console.log(req.body.title);
+	db.songlist.findAndModify({
+		query: {_id:mongojs.ObjectId(id)},
+		update: {$set: {
+			artist: req.body.artist,
+			album: req.body.album,
+			title: req.body.title 
+		}},
+		new: true},
+		function(err,doc){
+			res.json(doc);	
+	});
+});
 
 // routes ==================================================
 // pass our application into our routes
