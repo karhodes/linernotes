b5f0245d042c6681033a585138f3cffa1695df52
Kelly Rhodes <krhodes1@fullsail.edu>
2016-01-12 15:27:17 -0500

    changed from get to post

--- api/index.js
+++ api/index.js
@@ -6,8 +6,8 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
 
-router.get('/posts', function (req, res) {
-  res.send('Post Successful');
+router.post('/posts', function (req, res) {
+  res.post('Post Successful');
 })
 
 
