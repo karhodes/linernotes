a212e4d850214f5a4db3c94c8791c809d6e5f5d4
Kelly Rhodes <krhodes1@fullsail.edu>
2016-01-18 18:04:12 -0500

    working backend routes

--- api/routes/tracks.js
+++ api/routes/tracks.js
@@ -1,27 +1,39 @@
-'use strict';
+// LinerNotes application:  Routes for tracks
 
-/**
-* Module dependencies.
-*/
+module.exports = function () {
 
-module.exports = function (express) {
+	console.log("Hello from router!");
 
+	// Module dependencies
+	var path = require('path');
+	var express = require('express');
 	var router = express.Router();
-	var tracks = require('../../api/controllers/tracks');
+	var ejs = require('ejs');
 
+	// Controllers for tracks routes
+	var tracks = require('../controllers/tracks');
 
-// Tracks Routes
-router.route('/tracks')
-    .get(tracks.all)
-    .post(tracks.create);
-router.route('/tracks/:tracksId')
-    .get(tracks.show)
-    .put(tracks.update)
-    .delete(tracks.destroy);
+	// Tracks Routes
+	router.get('/', function(req,res){
+		res.send("Brandy is awesome!");
+	});
+
+	router.route('/')
+    	.get(tracks.all);
+
+    /*	
+    	.post(tracks.create);
+	router.route('/:trackId')
+    	.get(tracks.show)
+    	.put(tracks.update)
+    	.delete(tracks.destroy);
+    */	
+
 
 // Finish with setting up the trackId param
 // Note: the tracks.tracks function will be called everytime then it will call the next function.
-router.param('trackId', tracks.track);
+//router.param('trackId', tracks.track);
 
 return router;
+
 };
--- server.js
+++ server.js
@@ -3,11 +3,13 @@
 var express 	= require('express');
 var morgan 		= require('morgan');
 var app 		= express();
+var fs 			= require('fs');
 var path 		= require('path');
+var router 		= require('./api/routes/tracks.js');
 var port 		= process.env.PORT || 7000;
 
 // Logging request details (dev only)
-app.use(morgan('dev'))
+//app.use(morgan('dev'))
 
 // Use the template (partial) views
 app.use('/templates', express.static('templates'));
@@ -17,12 +19,14 @@ app.use('/templates', express.static('templates'));
 app.use(express.static(__dirname + '/public')); 
 
 // Allows the browser to GET the bower files
-app.use('/bower', express.static(__dirname + '/bower'));
+//app.use('/bower', express.static(__dirname + '/bower'));
 
 var db = require('./db.js');
 
+app.use('/tracks',router());
+
 // Routes (would like to glob...)
-app.use('/tracks', require('./api/routes/tracks.js')(express));
+//app.use('/tracks', require('./api/routes/tracks.js')(express));
 
 // Globbing routing files
 //   config.getGlobbedFiles('./api/routes/**/*.js').forEach(function(routePath) {
@@ -31,9 +35,9 @@ app.use('/tracks', require('./api/routes/tracks.js')(express));
 
 
 // Allows for navigation from other pages than homepage
-app.get('/', function(req, res){
-	res.render('index.html.ejs');
-})
+// app.get('/', function(req, res){
+// 	res.render('index.html.ejs');
+// })
 
 // Start The Server *************************************
 var server = app.listen(port, function() {
